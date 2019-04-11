d6ed39c74755930ffb09769fc7a34ebb171bef6d
mmcadet <34467088+mmcadet@users.noreply.github.com>
2019-04-11 10:41:43 -0500

    041111 edits

--- contact.php
+++ contact.php
@@ -23,6 +23,32 @@ else
 ?>
 
 
+<form id="contact-form" action="contact.php" method="post">
+        <ul>
+          <li>
+            <label for="name">Name</label>
+            <input type="text" id="name" name="name" placeholder="John Smith" required="required">
+          </li>
+          <li>
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email" placeholder="example@gmail.com" required="required">
+          </li>
+          <li>
+            <label for="message">Message</label>
+            <textarea id="message" name="message" required="required"></textarea>
+          </li>
+        </ul>
+        <input type="submit" name="formSubmit" value="Submit">
+      </form>
+
+
+
+
+
+
+
+
+
 
 
 
