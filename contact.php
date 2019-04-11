3a308ffbf08c726e7125047460a670831a6e7065
mmcadet <34467088+mmcadet@users.noreply.github.com>
2019-04-11 12:01:26 -0500

    041119 fixes

--- contact.php
+++ contact.php
@@ -1,55 +1,26 @@
-<?php
-        if($_POST['formSubmit'] == "Submit")
-        {
-          $varName = $_POST['formMovie'];
-          $varEmail = $_POST['email'];
-        }
-      ?>
+ <?php 
+        // if($_POST['formSubmit'] == "Submit")
+        // {
+        //   $varName = $_POST['formMovie'];
+        //   $varEmail = $_POST['email'];
+        // }
+    ?>
       <?php
-if($errorMessage != "") 
-{
-  echo("<p>There was an error:</p>\n");
-  echo("<ul>" . $errorMessage . "</ul>\n");
-} 
-else 
-{
-  $fs = fopen("mydata.csv","a");
-  fwrite($fs,$varName . ", " . $varMovie . "\n");
-  fclose($fs);
+// if($errorMessage != "") 
+// {
+//   echo("<p>There was an error:</p>\n");
+//   echo("<ul>" . $errorMessage . "</ul>\n");
+// } 
+// else 
+// {
+//   $fs = fopen("mydata.csv","a");
+//   fwrite($fs,$varName . ", " . $varName2 . "\n");
+//   fclose($fs);
 
-  header("Location: contact.html");
-  exit;
-}
-?>
-
-
-<form id="contact-form" action="contact.php" method="post">
-        <ul>
-          <li>
-            <label for="name">Name</label>
-            <input type="text" id="name" name="name" placeholder="John Smith" required="required">
-          </li>
-          <li>
-            <label for="email">Email</label>
-            <input type="email" id="email" name="email" placeholder="example@gmail.com" required="required">
-          </li>
-          <li>
-            <label for="message">Message</label>
-            <textarea id="message" name="message" required="required"></textarea>
-          </li>
-        </ul>
-        <input type="submit" name="formSubmit" value="Submit">
-      </form>
-
-
-
-
-
-
-
-
-
-
+//   header("Location: contact.html");
+//   exit;
+// }
+// ?> 
 
 
 <?php
@@ -57,25 +28,30 @@ if($_POST['formSubmit'] == "Submit")
 {
 	$errorMessage = "";
 	
-	if(empty($_POST['formMovie']))
-	{
-		$errorMessage .= "<li>You forgot to enter a movie!</li>";
-	}
 	if(empty($_POST['formName']))
 	{
-		$errorMessage .= "<li>You forgot to enter a name!</li>";
+		$errorMessage .= "<li>Please enter name</li>";
+	}
+	if(empty($_POST['formEmail']))
+	{
+		$errorMessage .= "<li>Please enter email</li>";
+	}
+	if(empty($_POST['formMessage']))
+	{
+		$errorMessage .= "<li>Please enter your message</li>";
 	}
 	
-	$varMovie = $_POST['formMovie'];
 	$varName = $_POST['formName'];
+	$varEmail = $_POST['formEmail'];
+	$varMessage = $_POST['formMessage'];
 
 	if(empty($errorMessage)) 
 	{
 		$fs = fopen("mydata.csv","a");
-		fwrite($fs,$varName . ", " . $varMovie . "\n");
+		fwrite($fs,$varName . ", " . $varEmail . ", " . $varMessage . "\n");
 		fclose($fs);
 		
-		header("Location: thankyou.html");
+		header("Location: contact.html");
 		exit;
 	}
 }
@@ -83,7 +59,7 @@ if($_POST['formSubmit'] == "Submit")
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
 <html>
 <head>
-	<title>My Form</title>
+	<title>Contact</title>
 </head>
 
 <body>
@@ -94,16 +70,36 @@ if($_POST['formSubmit'] == "Submit")
 			echo("<ul>" . $errorMessage . "</ul>\n");
 		} 
 	?>
-	<form action="myform1.php" method="post">
-		<p>
+	<!-- <form action="contact.php" method="post"> -->
+		<!-- <p>
 			What is your favorite movie?<br>
-			<input type="text" name="formMovie" maxlength="50" value="<?=$varMovie;?>" />
+			<input type="text" name="formMovie" maxlength="50" value="<?=$varName2;?>" />
 		</p>
 		<p>
 			What is your name?<br>
 			<input type="text" name="formName" maxlength="50" value="<?=$varName;?>" />
 		</p>				
 		<input type="submit" name="formSubmit" value="Submit" />
-	</form>
+	</form> -->
+
+	<form id="contact-form" action="contact.php" method="post">
+        <ul>
+          <li>
+            <label for="name">Name</label>
+            <input type="text" id="name" name="formName" placeholder="John Smith" required="required" maxlength="50" value="<?=$varName;?>"/>
+          </li>
+          <li>
+            <label for="email">Email</label>
+            <input type="email" id="email" name="formEmail" placeholder="example@gmail.com" required="required" maxlength="50" value="<?=$varEmail;?>"/>
+          </li>
+          <li>
+            <label for="message">Message</label>
+            <textarea id="message" name="formMessage" required="required" value="<?=$varMessage;?>"></textarea>
+          </li>
+        </ul>
+        <input type="submit" name="formSubmit" value="Submit">
+      </form>
+
+
 </body>
 </html>
